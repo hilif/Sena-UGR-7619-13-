19fdc9ddb4848b2a06bd215147a909bdab26c27e
hilif <119324167+hilif@users.noreply.github.com>
2023-01-27 19:38:22 +0300

    Add files via upload

--- scriptcompetition.js
+++ scriptcompetition.js
@@ -1,18 +1,3 @@
-const navToggler = document.querySelector(".nav-toggler");
- navToggler.addEventListener("click", navToggle);
-
- function navToggle() {
-    navToggler.classList.toggle("active");
-    const nav = document.querySelector(".nav");
-    nav.classList.toggle("open");
-    if(nav.classList.contains("open")){
-      nav.style.maxHeight = nav.scrollHeight + "px";
-    }
-    else{
-      nav.removeAttribute("style");
-    }
- }
-
 
  //selecting all required elements
 const dropArea = document.querySelector(".drag-area"),
@@ -66,35 +51,3 @@ function showFile(){
   }
 }
 
-/* Registration form validation */
-
-const name = document.getElementById('name')
-const email = document.getElementById('email')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
-
-form.addEventListener('submit', (e) => {
-  let messages = []
-  if (name.value === '' || name.value == null) {
-    messages.push('Name is required')
-  }
-})
-
-function validateEmail(email) {
-  let res = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return res.test(email);
-}
-function validate() {
-  let result = $("#result");
-  let email = $("#email").val();
-  result.text("");
-  if(validateEmail(email)) {
-    result.text(email + " is valid");
-    result.css("color", "blue");
-  } else {
-    result.text(email + " is not valid");
-    result.css("color", "red");
-  }
-  return false;
-}
-$("#validate").on("click", validate);
